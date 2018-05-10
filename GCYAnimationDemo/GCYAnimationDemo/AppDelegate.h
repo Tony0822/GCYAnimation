5a1f1a1bb58c1da98e9fe5a5ea4f76eb73a9813d
gaochongyang <gaochongyang@enmonster.com>
2018-05-10 10:07:11 +0800

    创建工程

---
+++ GCYAnimationDemo/GCYAnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCYAnimationDemo
+//
+//  Created by gaochongyang on 2018/5/10.
+//  Copyright © 2018年 gaochongyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
